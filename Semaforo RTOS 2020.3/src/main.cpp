a25847517f0f57086720e0d686f30a1e9d3e1b1d
Magnatto <renato.fdmferreira@gmail.com>
2020-12-22 20:14:41 -0300

    Implementação do thead de leitura de entrada

--- Semaforo RTOS 2020.3/src/main.cpp
+++ Semaforo RTOS 2020.3/src/main.cpp
@@ -2,73 +2,64 @@
 #include "ChRt.h"   //Biblioteca para uso do ChibiOS
 
 // Pino LED_BUILTIN no Arduino usualmente é o 13.
+
+// Pinos de Entrada
+#define PEDESTRE 2
+#define SECUNDARIO 3
+bool presencaPedestre = FALSE;
+bool presencaSecundario = FALSE;
+
+//Tamanho das threads
 #define tamanhoThread1 64 //Tamanho em bytes da memória alocada a thread
 #define tamanhoThread2 64
+#define tamanhoThread3 64
 
 uint8_t state = 0;
 
-
 //------------------------------------------------------------------------------
-// Thread 1, acende o LED
+// Thread 3, leitura da entrada
 //
-//Declara área de trabalho e tamanho da memória alocada a essa área
-static THD_WORKING_AREA(waThread1, tamanhoThread1); 
+//Lê as entradas dos sensores de pedestre e semáforo secundário e atualiza a cada segundo
+static THD_WORKING_AREA(waThread3, tamanhoThread3); 
 
-static THD_FUNCTION(Thread1, arg)
+static THD_FUNCTION(Thread3, arg)
 {//Declara a função do Thread
 
   (void)arg;
-  pinMode(LED_BUILTIN, OUTPUT);
+
   while(true)
     {//Loop infinito
-      if (state)
-      {
-        digitalWrite(LED_BUILTIN, HIGH);
-        chThdSleepMilliseconds(500);  //Período aproximado de piscada
-        state=0;
-        Serial.print("cyka ");
-      }
-      chThdSleepMilliseconds(1000); //Período aproximado da thread
-    }
-}
-//------------------------------------------------------------------------------
-// Thread 2, apaga o LED
-//
-//Declara área de trabalho e tamanho da memória alocada a essa área
-static THD_WORKING_AREA(waThread2, tamanhoThread2);
-
-
-static THD_FUNCTION(Thread2, arg)
-{//Declara a função do Thread
-  pinMode(LED_BUILTIN, OUTPUT);
-  (void)arg;
-  while(true)
-  {
-    if (!state)
-    {
+      presencaPedestre = digitalRead(PEDESTRE);
+      presencaSecundario = digitalRead(SECUNDARIO);
+      chThdSleepMilliseconds(500);
+      if(!presencaPedestre) Serial.println("Tem gente! ");
+      if(!presencaSecundario) Serial.println("Tem carro! ");
+      digitalWrite(LED_BUILTIN, HIGH);
+      chThdSleepMilliseconds(500);
       digitalWrite(LED_BUILTIN, LOW);
-      chThdSleepMilliseconds(500);  //Período aproximado de piscada
-      state=1;
-      Serial.print("blyat ");
     }
-    chThdSleepMilliseconds(1000); //Período aproximado da thread
-  }
 }
-//------------------------------------------------------------------------------
 
 // continua setup() ao chamar chBegin().
 void chSetup() {
   // Inicializa as threads.
-  chThdCreateStatic(waThread1, sizeof(waThread1),
+  /*chThdCreateStatic(waThread1, sizeof(waThread1),
     NORMALPRIO + 2, Thread1, NULL);
 
   chThdCreateStatic(waThread2, sizeof(waThread2),
     NORMALPRIO + 1, Thread2, NULL);
+*/
+  chThdCreateStatic(waThread3, sizeof(waThread3),
+    NORMALPRIO, Thread3, NULL);
 }
 //------------------------------------------------------------------------------
 void setup() {
-  // Inicialisa o OS e chama chSetup.
+  
   Serial.begin(9600);
+  pinMode(PEDESTRE, INPUT_PULLUP);
+  pinMode(SECUNDARIO, INPUT_PULLUP);
+
+  // Inicialisa o OS e chama chSetup.
   chBegin(chSetup);
   // chBegin() reseta as pilhas e nunca deve retornar.
   while (true) {}
